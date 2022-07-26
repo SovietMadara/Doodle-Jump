d489ec15fbe2dcfb28b7576b3bab6f881752276a
SovietMadara <arjunkulthe144@gmail.com>
2022-07-26 22:37:07 +0530

    Added Game Menu

--- Doodle Jump/Assets/Scripts/Score.cs
+++ Doodle Jump/Assets/Scripts/Score.cs
@@ -2,32 +2,18 @@ using UnityEngine;
 using UnityEngine.UI;
 
 
-public class Scores : MonoBehaviour
+public class Score : MonoBehaviour
 {
 
     public Transform player;
     public Text scoreText;
-    public float score;
-    public float currentScore;
+    
     
 
     // Update is called once per frame
     public void Update()
     {
-        currentScore = 0;
-        scoreText.text = currentScore.ToString("0");
-        score = player.position.y;
-
-        if(score > currentScore)
-        {
-            score = currentScore;
-            
-
-        }
-        else
-        {
-            currentScore = currentScore;
-        }
-        
+        scoreText.text = player.position.y.ToString("0");
     }
+    
 }
