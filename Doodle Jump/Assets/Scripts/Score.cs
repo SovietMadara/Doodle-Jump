1f17dc530110460f6624e88e880c82bf1b7e7b28
SovietMadara <arjunkulthe144@gmail.com>
2022-07-27 12:27:46 +0530

    Score Fixed

--- Doodle Jump/Assets/Scripts/Score.cs
+++ Doodle Jump/Assets/Scripts/Score.cs
@@ -7,13 +7,21 @@ public class Score : MonoBehaviour
 
     public Transform player;
     public Text scoreText;
+    public double scoreText1;
+    public Text finalScore;
+    public double finalScore1;
     
     
 
     // Update is called once per frame
     public void Update()
     {
-        scoreText.text = player.position.y.ToString("0");
+        //finalScore1=0.0;
+        scoreText1 = player.position.y;
+        if(finalScore1 !< scoreText1){
+            finalScore1=scoreText1; 
+            scoreText.text=finalScore1.ToString("0");
+        }
     }
     
 }
